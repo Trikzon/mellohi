d9b5b18ea7542349be568f9033e20fd88cbb385b
Dion Tryban <github@diontryban.com>
2023-08-04 03:10:35 -0400

    Add basic placing and breaking mechanics

--- addons/blocks/terrain/terrain.gd
+++ addons/blocks/terrain/terrain.gd
@@ -21,3 +21,33 @@ func _ready():
                 _chunk_instances[Vector3i(x, y, z)] = chunk_instance
                 
                 chunk_instance.generate()
+
+
+func set_block(block: Block, x: int, y: int, z: int):
+    set_blockv(block, Vector3i(x, y, z))
+
+
+func set_blockv(block: Block, global_pos: Vector3i):
+    var chunk_pos = global_to_chunk_pos(global_pos)
+    if _chunk_instances.has(chunk_pos):
+        var chunk: ChunkInstance = _chunk_instances[chunk_pos]
+        chunk.set_blockv(block, global_to_local_pos(global_pos))
+    # TODO: Do something if the chunk does not exist yet
+
+
+func global_to_local_pos(global_pos: Vector3i) -> Vector3i:
+    const chunk_size = BlocksPluginOptions.CHUNK_SIZE
+    return Vector3i(
+        posmod(global_pos.x, chunk_size),
+        posmod(global_pos.y, chunk_size),
+        posmod(global_pos.z, chunk_size)
+    )
+
+
+func global_to_chunk_pos(global_pos: Vector3i) -> Vector3i:
+    const chunk_size = BlocksPluginOptions.CHUNK_SIZE
+    return Vector3i(
+        floor(global_pos.x / chunk_size),
+        floor(global_pos.y / chunk_size),
+        floor(global_pos.z / chunk_size)
+    )
--- game/player/player_block_interactor.gd
+++ game/player/player_block_interactor.gd
@@ -8,7 +8,6 @@ func _physics_process(_delta):
     if is_colliding():
         var normal = get_collision_normal()
         var collision_pos = get_collision_point() - normal * 0.5
-        print(collision_pos)
         var block_x = floor(collision_pos.x) + 0.5
         var block_y = floor(collision_pos.y) + 0.5
         var block_z = floor(collision_pos.z) + 0.5
@@ -18,7 +17,17 @@ func _physics_process(_delta):
         block_outline.visible = true
         block_outline.global_rotation = Vector3()
         
-        # Break block at block_pos
-        # Place block at block_pos + normal
+        var air_block = Blocks.id_to_block("air")
+        var stone_block = Blocks.id_to_block("stone")
+        # Implement a better way to get the terrain.
+        # Should this be done in-plugin, or handled by the game?
+        var terrain: Terrain = get_node("/root/Main/Terrain")
+        if not terrain or not air_block or not stone_block:
+            return
+        
+        if Input.is_action_just_pressed("player_break"):
+            terrain.set_blockv(air_block, block_pos)
+        elif Input.is_action_just_pressed("player_place"):
+            terrain.set_blockv(stone_block, block_pos + normal)
     else:
         block_outline.visible = false
